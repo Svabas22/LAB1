51f3786ff065a0da585c42767e5a562922dd150e
Svabas22 <simonas.svabas2002@gmail.com>
2022-09-12 16:59:50 +0300

    Idetas mygtukas, kuris pakeicia teksta

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView txtMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.txtMain = findViewById(R.id.txtMain);
+    }
+
+    public void btnClick(View view) {
+        this.txtMain.setText("Tekstas pakeistas");
     }
 }

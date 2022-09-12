068d2ead26b47e606d9805948e95aa03280dee40
Svabas22 <simonas.svabas2002@gmail.com>
2022-09-12 17:30:23 +0300

    Idetas mygtukas, kuris pakeicia teksto spalva

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -9,13 +11,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private TextView txtMain;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.txtMain = findViewById(R.id.txtMain);
     }
 
-    public void btnClick(View view) {
+    public void btnClick1(View view) {
         this.txtMain.setText("Tekstas pakeistas");
+    }
+
+
+    public void btnClick2(View view) {
+        this.txtMain.setTextColor(Color.RED);
     }
 }
